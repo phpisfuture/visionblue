b0c17998ae3a8bd8f1d377ac8d998a9c3ffef62d
qi <948017056@qq.com>
2018-12-18 11:53:42 +0800

    hetian/LJWEBSITE-allCheck # 添加判断ie逻辑代码

---
+++ js/noIe.js
@@ -0,0 +1,9 @@
+if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE6.0") {
+    window.location.href = './noIe.html';
+} else if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE7.0") {
+    window.location.href = './noIe.html';
+} else if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE8.0") {
+    window.location.href = './noIe.html';
+} else if (navigator.appName == "Microsoft Internet Explorer" && navigator.appVersion.split(";")[1].replace(/[ ]/g, "") == "MSIE9.0") {
+    window.location.href = './noIe.html';
+}
