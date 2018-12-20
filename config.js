9866168fd401bb37559c79a699450e19e60f4c75
qi <948017056@qq.com>
2018-12-20 16:52:07 +0800

    hetian/LJWEBSITE-ajax # 添加首页ajax请求

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-sssss
+var api = '';
+var apiConfig = 'development';
+if(apiConfig == 'development') {
+  api = 'http://ladmin.zjchgkj.com'
+}else if (apiConfig == 'test') {
+  api = ''
+} else if (apiConfig == 'production') {
+  api = ''
+}
